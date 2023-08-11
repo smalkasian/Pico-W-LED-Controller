4623effc3e28e5ee435ae8bdefd8014f0dffb11f
Samuel Malkasian <malkasiangroup@gmail.com>
2023-08-11 10:26:57 -0500

    updated the logic and version check

--- build/PicoOS.py
+++ build/PicoOS.py
@@ -14,10 +14,15 @@
 # If you run into issues or need help with the code, please feel free to reach out!
 # malkasiangroup@gmail.com
 #
-#-----------------------------------CHANGELOG----------------------------------------
-__version__ = (1,3,0)
-status = print("UNSTABLE - Developer Preview")
-#
+#--------------------------------------------------------------------------------------
+print("UNSTABLE - Developer Preview")
+def deliver_current_version():
+    __version__ = (1,3,0)
+    is_stable = False
+    local_version = '.'.join(str(i) for i in __version__)
+    print(f"CURRENT VERSION: {local_version}")
+    return __version__
+#------------------------------------CHANGELOG-----------------------------------------
 # • If the wifi files doesn't exist, when the device first boots, it will create the file empty file.
 # • Adding feature for multiple wireless networks to be stored. Device will scan through till it finds one and connects.
 # • Web pages now pulls from remote file rather within the script.
@@ -76,7 +81,7 @@ blue.duty_u16(0)
 def yes_validator():
     userYesNo = input("[yes/no]: ")
     while userYesNo.lower() not in ['yes', 'no']:
-        userYesNo = input("Invalid entry: Please enter yes or no: ") #MAKE IT CHANGE THE INPUT TO .lower
+        userYesNo = input("Invalid entry: Please enter yes or no: ")
     else:
         if userYesNo == 'yes':
             print("")
@@ -93,19 +98,17 @@ def number_option_validator():
             print("")
             return 0
         elif user_choice == '2':
+            print("")
             return 1
         elif user_choice == '3':
             print("")
             return 2
         elif user_choice == '4':
+            print("")
             return 3
         if user_choice == '5':
             print("")
             return 4
-
-
-
-
 
 def lights_off():
     red.duty_u16(0)
@@ -116,38 +119,41 @@ def load_wifi_credentials():
     try:
         with open('wifipasswords.json', 'r') as file:
             networks = json.load(file)
-            #data = data_list[0] AUTOMATICALLY CHOOSE THE FIRST NETWORK IN THE LIST
             if isinstance(networks, list) and all(isinstance(network, dict) and 'ssid' in network and 'password' in network for network in networks):
                 return networks
             else:
-                raise ValueError("Invalid format or no passwords found in 'wifipasswords.json'")
+                raise ValueError("Invalid format in 'wifipasswords.json'")
     except (OSError, ValueError):
-        default_network = [{"ssid": "", "password": ""}]
         with open('wifipasswords.json', 'w') as file:
-            json.dump(default_network, file)
+            json.dump([], file)
         return []
 
-def connect_wifi():
-    try:
-        networks = load_wifi_credentials()
-        if not networks or not any(network["ssid"] and network["password"] for network in networks):
-            print("No valid Wi-Fi networks found.")
-            return
-
-        station = network.WLAN(network.STA_IF)
-        for network_info in networks:
-            ssid = network_info["ssid"]
-            password = network_info["password"]
-
-            if not ssid or not password:
-                print("Invalid Wi-Fi credentials found. Skipping this network.")
-                continue
-
+def update_wireless_password(ssid, password):
+        global station
+        lights_off()
+        red.duty_u16(50000)
+        print("")
+        print("")
+        print("Current SSID & Password:")
+        print("--------------------------------")
+        print(f"SSID: {ssid}")
+        print(f"Password: {password}")
+        print("--------------------------------")
+        print("")
+        print("Would you like to update the password?")
+        update_credentials = yes_validator()
+        if update_credentials == "yes":
+            new_ssid = input("Enter your WiFi SSID: ")
+            new_password = input("Enter your WiFi password: ")
+            data["ssid"] = new_ssid
+            data["password"] = new_password
+            with open('wifipasswords.json', "w") as file:
+                json.dump(data, file)
             station.active(True)
             station.connect(ssid, password)
-
-            max_retries = 50
+            lights_off()
             pulse_direction = 10
+            max_retries = 50
             brightness = 0
             while not station.isconnected() and max_retries > 0:
                 brightness += pulse_direction * 1000
@@ -158,83 +164,11 @@ def connect_wifi():
                     brightness = 0
                     pulse_direction = 5
                 red.duty_u16(brightness)
-                green.duty_u16(int(brightness * 0.5))
+                green.duty_u16(int(brightness*0.5))
                 blue.duty_u16(0)
                 max_retries -= 1
                 time.sleep(0.1)
-            if station.isconnected():
-                lights_off()
-                print('Connection successful!')
-                print(station.ifconfig())
-                for i in range(10):
-                    green.duty_u16(65025)
-                    time.sleep(0.1)
-                    green.duty_u16(0)
-                    time.sleep(0.1)
-                green.duty_u16(65025)
-                time.sleep(2)
-                lights_off()
-                return
-            else:
-                print(f"Connection to {ssid} failed.")
-        # If no successful connection is made to any network
-        print("Connection Failed: Unable to connect to any Wi-Fi network.")
-        red.duty_u16(0)
-        green.duty_u16(0)
-        blue.duty_u16(0)
-        for i in range(5):
-            red.duty_u16(65025)
-            time.sleep(0.1)
-            red.duty_u16(0)
-            time.sleep(0.1)
-            red.duty_u16(0)
-        lights_off()
-        red.duty_u16(50000)
-        update_wireless_password()
-    except KeyError:
-        print("Connection Failed: No Valid Password Record")
-        update_wireless_password()
-        lights_off()
-    except Exception as e:
-        print("Connection Failed: An exception occurred -", e)
-        print("System will now shut down.")
-        lights_off()
-
-def update_wireless_password(): 
-        try:
-            with open('wifipasswords.json', 'r') as file:
-                data_list = json.load(file)
-        finally:
-            for i, data in enumerate(data_list):
-                print(f"Network {i + 1}:")
-                print(f"SSID: {data['ssid']}")
-                print(f"Password: {data['password']}")
-                print("--------------------------------")
-            print("Which network do you want to connect to?")
-            choose_index = number_option_validator()
-            connect_wifi()
-        try:
-            print("Do you want to update the password?")
-            update_credentials = yes_validator()
-        except KeyError:
-            print("No exisiting wifi credentials.")
-            print("Would you like to add a new network?")
-            update_credentials = yes_validator()
-        except OSError:
-            print("A password might be incorrect.")
-            print("Would you like to add a new network?")
-            update_credentials = yes_validator()
-        #It's not triggering a traceback error, so you need it to update the password
-        #If in the event it doesn't trigger a traceback.
-        if update_credentials == "yes":
-            new_ssid = input("Enter your WiFi SSID: ")
-            new_password = input("Enter your WiFi password: ")
-            data_list[choose_index]["ssid"] = new_ssid
-            data_list[choose_index]["password"] = new_password
-            with open('wifipasswords.json', "w") as file:
-                json.dump(data, file)
-            lights_off()
-            connect_wifi()
+            update_wireless_password(new_ssid, new_password)
         if update_credentials == "no":
             print("Device will not work without WiFi.")
             print("Powering Down.")
@@ -245,13 +179,79 @@ def update_wireless_password():
                 red.duty_u16(0)
                 time.sleep(0.1)
                 red.duty_u16(0)
+            station.disconnect()
+            station.active(False)
             lights_off()
-        
+
+def connect_wifi():
+    station = network.WLAN(network.STA_IF)
+    red = PWM(Pin(0))
+    green = PWM(Pin(1))
+    blue = PWM(Pin(2))
+    red.freq(1000)
+    green.freq(1000)
+    blue.freq(1000)
+    try:
+        with open('wifipasswords.json') as file:
+            data = json.load(file)
+            ssid = data["ssid"]
+            password = data["password"]
+        station.active(True)
+        station.connect(ssid, password)
+        max_retries = 50
+        pulse_direction = 10
+        brightness = 0
+        while not station.isconnected() and max_retries > 0:
+            brightness += pulse_direction * 1000
+            if brightness >= 50000:
+                brightness = 50000
+                pulse_direction = -5
+            elif brightness <= 0:
+                brightness = 0
+                pulse_direction = 5
+            red.duty_u16(brightness)
+            green.duty_u16(int(brightness * 0.5))
+            blue.duty_u16(0)
+            max_retries -= 1
+            time.sleep(0.1)
+        if station.isconnected():
+            lights_off()
+            print('Connection successful!')
+            print(station.ifconfig())
+            for i in range(10):
+                green.duty_u16(65025)
+                time.sleep(0.1)
+                green.duty_u16(0)
+                time.sleep(0.1)
+            green.duty_u16(65025)
+            time.sleep(2)
+            lights_off()
+        else:
+            print("Connection Failed: No specific reason identified")
+            red.duty_u16(0)
+            green.duty_u16(0)
+            blue.duty_u16(0)
+            for i in range(5):
+                red.duty_u16(65025)
+                time.sleep(0.1)
+                red.duty_u16(0)
+                time.sleep(0.1)
+                red.duty_u16(0)
+            update_wireless_password(ssid, password)
+
+    except KeyError:
+        print("Connection Failed: No Valid Password Record")
+        update_wireless_password(ssid, password)
+    except Exception as e:
+        print("Connection Failed: An exception occurred -", e)
+
+station = network.WLAN(network.STA_IF)
+connect_wifi()
+
+
+
+
 #------------------------------------FUNCTIONS---------------------------------------
-def deliver_current_version(__version__):
-    local_version = '.'.join(str(i) for i in __version__)
-    print(f"CURRENT VERSION: {local_version}")
-    return __version__
 
 def set_brightness(brightnessChoice):
     global brightness
@@ -397,9 +397,12 @@ def parse_request(request):
     return ''
 
 def main_thread():
-    deliver_current_version(__version__, status)
     while True:
         try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', 80))
+            s.listen(5)
             conn, addr = s.accept()
             conn.settimeout(3.0)
             print('Received HTTP GET connection request from %s' % str(addr))
@@ -425,8 +428,6 @@ def main_thread():
 
 
 #---------------------------------MAIN PROGRAM------------------------------------------
-
-connect_wifi()
 
 def pico_os_main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- build/main.py
+++ build/main.py
@@ -4,48 +4,58 @@
 #------------------------------------------------------------------------------------
 # DO NOT DELETE THIS FILE - SYSTEM WILL NOT BOOT OR RUN.
 #------------------------------------------------------------------------------------
-
 import gc
 import urequests
-import ure as re
-from PicoOS import connect_wifi, pico_os_main
+import re
+import _thread
+from PicoOS import connect_wifi, pico_os_main, deliver_current_version
 
 gc.collect()
 connect_wifi()
 
+# LOGIC FLOW:
+# Connect Wifi
+# Get system up and running
+# Use additional thread to check for update (if update False, check in 7 days.)
+    # If no update, return "System up to date" and no button shows on web page.
+    # If update found, then "update system to" "curent OS __version" appears.
 # def deliver_current_version(__version__):
 #     local_version = '.'.join(str(i) for i in __version__)
 #     print(f"CURRENT VERSION: {local_version}")
 #     return __version__
 
-#Check IF STABLE TRUE OR FALSE! Just another check after update has been determined it DOES need to be updated.
 
-# local_version = deliver_current_version(__version__)
-remote_version_url = 'https://raw.githubusercontent.com/smalkasian/Pico-W-LED-Controller/main/build/PicoOS.py'
 
 def get_version_number(remote_version_url):
-    response = urequests.get(remote_version_url)
-    if response.status == 200:
-        match = re.search(r'__version__ = \((\d+,\d+,\d+)\)', response.text)
-        if match:
-            return match.group(0)
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = urequests.get(remote_version_url, headers=headers)
+        if response.status_code == 200:
+            match = re.search(r'__version__ = \((\d+,\d+,\d+)\)', response.text)
+            if match:
+                version_string = match.group(1)  # Extract the version string
+                version_components = tuple(map(int, version_string.split(',')))
+                return version_components
+    except Exception as e:
+        print("Error:", e)
     return None
 
-# print("Local Version: " + __version__)
-print("Remote Version: " + str(get_version_number(remote_version_url)))
 
+try:
+    local_version = deliver_current_version()
+    remote_version_url = 'http://raw.githubusercontent.com/smalkasian/Pico-W-LED-Controller/main/build/PicoOS.py'
+    remote_version = get_version_number(remote_version_url)
+    response = urequests.get(remote_version_url)
+    print("Status Code:", response.status_code)
 
-# I need the function to check what's in the PicoOS and update that here.
-# It then checks the file in the main foder and compares. It doesn't update
-# locally in main.py until the file is successfully downloaded. Everything
-# within those programs need to be updated. The only issue is that
-# everything needs to be a function. I need to make the entire file run 
-# under a single function. "PicoOS.run()"
+    if remote_version:
+        print(f"Current OS version: {local_version}")
+        print(f"There is an update available ({remote_version}).")
+    else:
+        print("Update check failed. Try again later.")
+except Exception as e:
+    print("Error:", e)
 
-
-# scripts for OTA updates here
-# scripts for running main OS in here
-# Instructions on how to run the system in here
 
 #------------------------------------------------------------------------------------
 #----------------------------------MAIN PROGRAM---------------------------------------
